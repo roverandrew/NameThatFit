3f74eec3502e25f862b0f97c9885343f477baf71
Andrew Rover <andrewrover@andrews-air-2.wireless.concordia.ca>
2020-01-14 16:43:47 -0500

    Add gitignore, procfile, update ports

--- app.js
+++ app.js
@@ -57,6 +57,11 @@ app.use("/outfits", outfitRoutes);
 app.use("/outfits/:id/comments", commentRoutes);
 app.use(indexRoutes);
 
-app.listen(3000, function(){
-   console.log("The fashion Server Has Started!");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
+   console.log("Server has started succesfully!");
 });
